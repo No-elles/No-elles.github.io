b2807eae27712f9d9a9bc20a64ae2cc30599c28f
No-elles <47335947+No-elles@users.noreply.github.com>
2019-04-06 16:52:46 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+  $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 1772) {
+      $("#nav-y1970s > .years").addClass("big-nav-text");
+    } else if (($(window).scrollTop() > 1772) {
+
+    }
+  });
+
+  $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 1772) {
+      $("#nav-y1960s > .years").addClass("big-nav-text");
+    } else if (($(window).scrollTop() > 1772) {
+
+    }
+  });
+
+  $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 1772) {
+      $("#nav-y1940s > .years").addClass("big-nav-text");
+    } else if (($(window).scrollTop() > 1772) {
+
+    }
+  });
+
+  $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    if ($(window).scrollTop() > 1772) {
+      $("#nav-y1930s > .years").addClass("big-nav-text");
+    } else if (($(window).scrollTop() > 1772) {
+
+    }
+  });
